a04efb69777dc4354da65a457a731fd7e3c8bf68
Cristina Solana <cs@nightshiftcreative.com>
2013-06-08 13:34:47 -0400

    Nested css output

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested #:compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -22,9 +22,3 @@ line_comments = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
-
-# on_stylesheet_saved do |file|
-# 	text = File.read(file)
-# 	puts = text.gsub("@", "@@")
-# 	File.open(file, "w") { |file| file << puts }
-# end
